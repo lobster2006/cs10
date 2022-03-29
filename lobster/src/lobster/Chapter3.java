03c65fa477e66d0c04c849baff8b7454082ea52a
16193001 <16193001@L815WD220021.edu.cbe.ab.ca>
2022-03-29 15:11:40 -0600

    cs 10 credit 3 java test

---
+++ lobster/src/lobster/Chapter3.java
@@ -0,0 +1,10 @@
+package lobster;
+
+public class Chapter3 {
+
+	public static void main(String[] args) {
+		// Tarun Auto-generated method stub
+
+	}
+
+}
---
+++ lobster/src/lobster/Credit3.java
@@ -0,0 +1,10 @@
+package lobster;
+
+public class Credit3 {
+
+	public static void main(String[] args) {
+		// "Tarun"
+
+	}
+
+}
