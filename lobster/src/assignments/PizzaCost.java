04a3d4da0acb0a90412cc95b17dc0001d80c6ec8
16193001 <16193001@L815WD220021.edu.cbe.ab.ca>
2022-04-21 15:11:22 -0600

    push to git

--- lobster/src/assignments/PizzaCost.java
+++ lobster/src/assignments/PizzaCost.java
@@ -1,40 +1,41 @@
 package assignments;
-
 import java.util.Scanner;
 
 public class PizzaCost {
-	/*
 
-	Program: ${PizzaCost}.java          Date: ${4/15/2022}
-
-
-	Author: Tarun Jaikrishnan 
-	School: CHHS
-	Course: Computer Science 10
-	 
-
-	*/
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		 // Declare variables
+		/*
+
+		Program: ${PizzaCost}.java          Date: ${4/15/2022}
+
+
+		Author: Tarun Jaikrishnan 
+		School: CHHS
+		Course: Computer Science 10
+		 
+
+		*/
+		@SuppressWarnings("resource")
 		
-        Scanner sc = new Scanner(System.in);
-        double size = 0.0;
-        double cost = 0.0;
-        
-        // Display instructions
-        System.out.println("\nEnter the size of the pizza(diameter): ");
-        size = sc.nextDouble();
-        
-        // Calculate total cost
-        cost = ((.05 * size * size) + 1 + .75);
-        
-        // Output the total
-        System.out.println("Total cost: $" + cost);	
+			 // Declare variables
+			
+	        Scanner sc = new Scanner(System.in);
+	        double size = 0.0;
+	        double cost = 0.0;
+	        
+	        // Display instructions
+	        System.out.println("\nEnter the size of the pizza(diameter): ");
+	        size = sc.nextDouble();
+	        
+	        // Calculate total cost
+	        cost = ((.05 * size * size) + 1 + .75);
+	        
+	        // Output the total
+	        System.out.println("Total cost: $" + cost);	
+
+		}
 
 	}
-
-}
-/* Screen Dump
-Enter the size of the pizza(diameter):
-*/
+	/* Screen Dump
+	Enter the size of the pizza(diameter):
+	*/
