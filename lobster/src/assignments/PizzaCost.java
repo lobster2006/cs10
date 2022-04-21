b1b96ee6fc8f8d09d6fc6845d4363826eca5428f
16193001 <16193001@L815WD220021.edu.cbe.ab.ca>
2022-04-21 14:50:34 -0600

    # WARNING: head commit changed in the meantime
    Signed-off-by: 16193001 <16193001@L815WD220021.edu.cbe.ab.ca>

---
+++ lobster/src/assignments/PizzaCost.java
@@ -0,0 +1,40 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class PizzaCost {
+	/*
+
+	Program: ${PizzaCost}.java          Date: ${4/15/2022}
+
+
+	Author: Tarun Jaikrishnan 
+	School: CHHS
+	Course: Computer Science 10
+	 
+
+	*/
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		 // Declare variables
+		
+        Scanner sc = new Scanner(System.in);
+        double size = 0.0;
+        double cost = 0.0;
+        
+        // Display instructions
+        System.out.println("\nEnter the size of the pizza(diameter): ");
+        size = sc.nextDouble();
+        
+        // Calculate total cost
+        cost = ((.05 * size * size) + 1 + .75);
+        
+        // Output the total
+        System.out.println("Total cost: $" + cost);	
+
+	}
+
+}
+/* Screen Dump
+Enter the size of the pizza(diameter):
+*/
